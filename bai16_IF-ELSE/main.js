41372b6493ed24a9e511d7b0d078369bf1cd1614
NgoAn <anngo16022006@gmail.com>
2026-01-11 22:33:10 +0700

    day12

---
+++ bai16_IF-ELSE/main.js
@@ -0,0 +1,9 @@
+// Câu lệnh if-else
+let dtb = Number(prompt("Nhập điểm trung bình: "));
+
+// Kiểm tra điều kiện
+if (dtb >= 5.0) {
+  console.log("Bạn đã đỗ");
+} else {
+  console.log("Bạn đã rớt");
+}
